a44df9e3bff085a08c4d09383d106dc06e368750
nakashima <nakashima@biocomputing.cc>
2019-09-22 13:34:27 +0900

    postsの外部キー追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  resources :users, only: [:show] do
-    resources :posts
-  end
+  resources :posts
 end
--- db/migrate/20190922035648_change_user_id_of_posts.rb
+++
@@ -1,9 +0,0 @@
-class ChangeUserIdOfPosts < ActiveRecord::Migration[5.2]
-  def up
-    change_column :posts, :user_id, :integer, null: false
-  end
-
-  def down
-    remove_column :posts, :user_id
-  end
-end
