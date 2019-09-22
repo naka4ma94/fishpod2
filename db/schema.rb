e14421e5f7f698d76020bfaacaf0e0eba1c6af3a
nakashima <nakashima@biocomputing.cc>
2019-09-22 14:21:13 +0900

    areaモデル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_22_034928) do
+ActiveRecord::Schema.define(version: 2019_09_22_051818) do
+
+  create_table "areas", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "image", null: false
