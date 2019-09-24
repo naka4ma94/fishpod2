254414b493869055a85f758e992f8b2e9aaa9a4c
nakashima <nakashima@biocomputing.cc>
2019-09-24 19:37:41 +0900

    posts#indexデザイン

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  process resize_to_limit: [400, 400]
+  process resize_to_limit: [100, 100]
 
   if Rails.env.production?
     storage :fog
