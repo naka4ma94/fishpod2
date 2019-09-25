0e38e83a7bffac5307b4818b1675f9d344e99abe
nakashima <nakashima@biocomputing.cc>
2019-09-26 03:00:42 +0900

    画像回転バグ修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -2,6 +2,14 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_fill: [1536, 2048, "Center"]
 
+  def fix_rotate
+      manipulate! do |img|
+          img = img.auto_orient
+          img = yield(img) if block_given?
+          img
+      end
+  end
+
   if Rails.env.production?
     storage :fog
   else
