4b80a989daf4610951189d9f9cde04643e531a60
nakashima <nakashima@biocomputing.cc>
2019-10-02 17:03:36 +0900

    toastr導入

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   validates :image, presence: true
-  validates :size, presence: true
+  validates :size, presence: true, length: { maximum: 2 }
+  validates :comment, length: { maximum: 150 }
   mount_uploader :image, ImageUploader
   default_scope -> { order(created_at: :desc) }
   belongs_to :user
