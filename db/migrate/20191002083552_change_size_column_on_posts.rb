0fc8a46b2484164691c770cfcad263aa39e53a9e
nakashima <nakashima@biocomputing.cc>
2019-10-02 17:38:46 +0900

    sizeカラム変更

--- db/migrate/20191002083552_change_size_column_on_posts.rb
+++ db/migrate/20191002083552_change_size_column_on_posts.rb
@@ -1,5 +1,5 @@
 class ChangeSizeColumnOnPosts < ActiveRecord::Migration[5.2]
   def change
-    change_column :posts, :size, :integer, null: false
+    change_column :posts, :size, 'integer USING CAST(column_name AS integer)'
   end
 end
