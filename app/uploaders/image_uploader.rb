8e7300ad515d9ac8fca2c24cd7160d33527e5b40
nakashima <nakashima@biocomputing.cc>
2019-10-03 16:49:18 +0900

    s3保存形式

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_fill: [1536, 2048, "Center"]
-  process :fix_exif_rotation
 
   if Rails.env.production?
     storage :fog
@@ -15,13 +14,5 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   def extension_whitelist
     %w(jpg jpeg gif png)
-  end
-
-  def fix_exif_rotation
-    manipulate! do |img|
-      img.auto_orient
-      img = yield(img) if block_given?
-      img
-    end
   end
 end
