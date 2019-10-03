893a7e70d7ccffb3a54fe13477a4c5a7d49d07f3
nakashima <nakashima@biocomputing.cc>
2019-10-03 17:29:42 +0900

    s3保存形式

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,4 +15,15 @@ class ImageUploader < CarrierWave::Uploader::Base
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
+
+  def filename
+    file.content_type = "image/JPG" if file.content_type == "image/jpeg"
+     "#{secure_token(10)}.#{file.extension}" if original_filename.present?
+  end
+
+  protected
+  def secure_token(length=16)
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
+  end
 end
