65e453c972678360f53fefbd4bf2a6d50fa07490
nakashima <nakashima@biocomputing.cc>
2019-10-03 17:40:53 +0900

    s3保存形式

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,6 +17,12 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   def filename
-    original_filename
+     "#{secure_token(10)}.#{file.jpg}" if original_filename.present?
+  end
+
+  protected
+  def secure_token(length=16)
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
   end
 end
