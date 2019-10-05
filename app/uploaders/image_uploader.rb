c2247de02797eb6a6c3f619b283b38335ee01937
nakashima <nakashima@biocomputing.cc>
2019-10-05 14:32:16 +0900

    画像回転

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -15,14 +15,4 @@ class ImageUploader < CarrierWave::Uploader::Base
   def extension_whitelist
     %w(jpg jpeg gif png)
   end
-
-  def filename
-     "#{secure_token(10)}.JPG" if original_filename.present?
-  end
-
-  protected
-  def secure_token(length=16)
-    var = :"@#{mounted_as}_secure_token"
-    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.hex(length/2))
-  end
 end
