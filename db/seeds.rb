d74d23a86f7329b741d53779d08617220b14bcaa
nakashima <nakashima@biocomputing.cc>
2019-10-06 15:22:58 +0900

    seedsにデータ挿入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+names = [
+  "竹山ダム",
+  "薩摩湖",
+  "鰻池",
+  "野池",
+  "住吉池",
+  "大隅湖",
+]
+
+names.each do |name|
+  User.create!(
+    { name: name }
+  )
