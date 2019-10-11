b2fda3b3421365359d15aa6dd956e2bf30caa817
nakashima <nakashima@biocomputing.cc>
2019-10-12 01:47:19 +0900

    関連するcommentも削除

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ApplicationRecord
 
   belongs_to :user
   belongs_to :area
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
 
